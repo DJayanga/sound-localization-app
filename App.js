9dca2724fc4c67a54d0cb8ccf1df1d67b785db0b
D_Jayan <dasun@surge.global>
2022-08-02 11:33:30 +0530

    2nd commit

--- App.js
+++ App.js
@@ -1,11 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import MapView, { Circle, Marker } from 'react-native-maps';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView style={styles.map} 
+         initialRegion={{
+          latitude: 7.318237,
+          longitude: 80.626133,
+          latitudeDelta: 0.0005,
+          longitudeDelta: 0.0005,
+        }}
+      >
+        <Marker
+        coordinate={{latitude: 7.318237, longitude: 80.626133}}
+        title="Coordinate 01"
+        pinColor='red'
+        ></Marker>
+        {/* <Circle center={{latitude: 7.318237, longitude: 80.626133}}
+        radius={100}
+        ></Circle> */}
+    
+        <Marker
+        coordinate={{latitude: 7.317180, longitude: 80.626346}}
+        title="Coordinate 02"
+        pinColor='red'
+        ></Marker>
+        {/* <Circle center={{latitude: 7.317335, longitude: 80.626346}}
+        radius={100}
+        ></Circle> */}
+
+        <Marker
+        coordinate={{latitude: 7.317503, longitude: 80.626910}}
+        title="Coordinate 03"
+        pinColor='red'
+        ></Marker>
+        {/* <Circle center={{latitude: 7.317503, longitude: 80.626910}}
+        radius={100}
+        ></Circle> */}
+
+<Marker
+        coordinate={{latitude: 7.317400, longitude: 80.626200}}
+        title="Sound Source"
+        pinColor='blue'
+        ></Marker>
+        {/* <Circle center={{latitude: 7.317335, longitude: 80.626346}}
+        radius={100}
+        ></Circle> */}
+      </MapView>
     </View>
   );
 }
@@ -16,5 +59,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
   },
 });
